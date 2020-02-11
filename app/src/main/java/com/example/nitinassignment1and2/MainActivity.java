3b59b7a3dcc6343220725653583f428ab16e301d
Nitin Jaswal <nathanjaswal@gmail.com>
2020-02-11 14:56:13 -0500

    [Nitin] First Commit

---
+++ app/src/main/java/com/example/nitinassignment1and2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.nitinassignment1and2;
+
+
+//
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
